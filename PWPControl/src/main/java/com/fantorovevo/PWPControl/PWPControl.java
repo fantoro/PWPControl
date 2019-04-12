3b199a6f6fcf0ccbb5241f66ad658ea2d15f4e73
fantoro <fantoro@outlook.com>
2019-04-12 13:10:34 +0200

    Added the GPL3 license and removed an unused class

--- PWPControl/src/main/java/com/fantorovevo/PWPControl/PWPControl.java
+++ PWPControl/src/main/java/com/fantorovevo/PWPControl/PWPControl.java
@@ -1,3 +1,24 @@
+/*
+ *  This file is part of PWPControl.
+ *
+ *  PWPControl is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PWPControl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with PWPControl.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ *  Copyright (C) 2019 fantoro
+ * 
+ *  fantoro@outlook.com
+ * 
+*/
 package com.fantorovevo.PWPControl;
 
 import java.util.logging.Logger;
